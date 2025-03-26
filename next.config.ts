550c2aa0d450995329d25cadb9520a6318810002
zce <w@zce.me>
2025-03-26 08:27:48 +0800

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,14 @@ import { build } from 'velite'
 
 import type { NextConfig } from 'next'
 
-export default async (): Promise<NextConfig> => {
-  const isDev = process.argv.indexOf('dev') !== -1
-  const isBuild = process.argv.indexOf('build') !== -1
-  if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-    process.env.VELITE_STARTED = '1'
-    await build({ watch: isDev, clean: !isDev })
-  }
-
-  return {
-    output: "export"
-  }
+console.log('process.argv', process.argv)
+const isDev = process.argv.indexOf('dev') !== -1
+const isBuild = process.argv.indexOf('build') !== -1
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = '1'
+  build({ watch: isDev, clean: !isDev })
 }
+
+export default {
+  output: "export"
+} satisfies NextConfig
